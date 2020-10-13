a6077a94653658a97709cb6d3f7a83dee39e316f
Szőllősi Viktor <szollosiviktor1975@gmail.com>
2020-10-13 09:26:46 +0200

    Square osztály létrehozása alap metódusokkal.

---
+++ src/gitnetbeansdemo/Square.java
@@ -0,0 +1,35 @@
+
+package gitnetbeansdemo;
+
+
+public class Square {
+    
+    private double side;
+    
+    public Square(double side){
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double area(double side){
+        return side*side;
+    }
+    
+    public double  perimeter(){
+        return side*4;
+    }
+    
+    public double getArea(){
+        return  side*side;
+    }
+    public double getPerimeter(){
+        return  side*4;
+    }
+}
