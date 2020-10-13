81ab8c8898973a07e58ab641cb9342c207f9d9c8
Szőllősi Viktor <szollosiviktor1975@gmail.com>
2020-10-13 09:24:45 +0200

    Projekt létrehozása.

---
+++ src/gitnetbeansdemo/GitNetbeansDemo.java
@@ -0,0 +1,17 @@
+
+package gitnetbeansdemo;
+
+
+public class GitNetbeansDemo {
+
+    
+    public static void main(String[] args) {
+
+        Square square = new Square(5);
+        
+        System.out.println(square.getArea());
+        System.out.println(square.getPerimeter());
+        
+    }
+    
+}
